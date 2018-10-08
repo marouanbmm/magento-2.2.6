6e449f8682c327faddc67473eac2620113092e21
Ben Mansour Marouan <marouan.benmansour@symdrik.com>
2018-10-08 14:11:54 +0100

    Add shipping methode

---
+++ app/code/MageTun/Checkout/Model/Carrier/MageTun.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * extensions from hundreds of the Import And Export Products extensions in the market as derived from Mageplaza Ranking which is using Magento developement Tunisia scores, rating reviews, search results, social metrics. 
+ * Copyright (C) 2017  Magento developement Tunisia 2018
+ * 
+ * This file is part of MageTun/Checkout.
+ * 
+ * MageTun/Checkout is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace MageTun\Checkout\Model\Carrier;
+
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Shipping\Model\Rate\Result;
+
+class MageTun extends \Magento\Shipping\Model\Carrier\AbstractCarrier implements
+    \Magento\Shipping\Model\Carrier\CarrierInterface
+{
+
+    protected $_code = 'magetun';
+
+    protected $_isFixed = true;
+
+    protected $_rateResultFactory;
+
+    protected $_rateMethodFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory
+     * @param \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $rateMethodFactory
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory,
+        \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $rateMethodFactory,
+        array $data = []
+    ) {
+        $this->_rateResultFactory = $rateResultFactory;
+        $this->_rateMethodFactory = $rateMethodFactory;
+        parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function collectRates(RateRequest $request)
+    {
+        if (!$this->getConfigFlag('active')) {
+            return false;
+        }
+
+        $shippingPrice = $this->getConfigData('price');
+
+        $result = $this->_rateResultFactory->create();
+
+        if ($shippingPrice !== false) {
+            $method = $this->_rateMethodFactory->create();
+
+            $method->setCarrier($this->_code);
+            $method->setCarrierTitle($this->getConfigData('title'));
+
+            $method->setMethod($this->_code);
+            $method->setMethodTitle($this->getConfigData('name'));
+
+            if ($request->getFreeShipping() === true || $request->getPackageQty() == $this->getFreeBoxes()) {
+                $shippingPrice = '0.00';
+            }
+
+            $method->setPrice($shippingPrice);
+            $method->setCost($shippingPrice);
+
+            $result->append($method);
+        }
+
+        return $result;
+    }
+
+    /**
+     * getAllowedMethods
+     *
+     * @param array
+     */
+    public function getAllowedMethods()
+    {
+        return ['flatrate' => $this->getConfigData('name')];
+    }
+}
